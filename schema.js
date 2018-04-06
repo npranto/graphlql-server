c0ded12b0d6a445db220b23a14bb7775241119aa
Nazmuz Shakib Pranto <npranto@gmail.com>
2018-04-06 16:48:28 -0400

    adds json-server to fetch json data through graphql

--- schema.js
+++ schema.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const {
 	GraphQLObjectType,
 	GraphQLString,
@@ -8,8 +10,6 @@ const {
 	GraphQLNonNull
 } = require('graphql');
 
-// mock data
-
 const CUSTOMERS = [
 	{ id: '1',	name: 'Kevin Williams', 	email: 'kwilliams@gmail.com', 	age: 15 },
 	{ id: '2', 	name: 'James Brown', 		email: 'jbrown@gmail.com', 		age: 20 },
@@ -19,11 +19,11 @@ const CUSTOMERS = [
 	{ id: '5',	name: 'Raja Ball', 			email: 'rball@gmail.com', 		age: 46 }
 ]
 const PRODUCTS = [
-	{ id: 1, name: 'Chair', 	price: 15.99 },
-	{ id: 2, name: 'Pen', 		price: 2 } ,
-	{ id: 3, name: 'Fan', 		price: 29.99 } ,
-	{ id: 4, name: 'Laptop', 	price: 1099.99 } ,
-	{ id: 5, name: 'Pillow', 	price: 20 }  
+	{ id: 1, name: 'Chair', 	price: 15.99 	},
+	{ id: 2, name: 'Pen', 		price: 2 		} ,
+	{ id: 3, name: 'Fan', 		price: 29.99 	} ,
+	{ id: 4, name: 'Laptop', 	price: 1099.99	} ,
+	{ id: 5, name: 'Pillow', 	price: 20 		}  
 ]
 
 const CustomerType = new GraphQLObjectType({
@@ -59,8 +59,38 @@ const ProductType = new GraphQLObjectType({
 	}
 })
 
-const RootQuery = new GraphQLObjectType({
-	name: 'RootQueryType',
+const PersonType = new GraphQLObjectType({
+	name: 'Person',
+	fields: {
+		id: {
+			type: GraphQLString
+		},
+		firstName: {
+			type: GraphQLString
+		},
+		lastName: {
+			type: GraphQLString
+		},
+		age: {
+			type: GraphQLInt
+		}
+	}
+})
+
+const TodoType = new GraphQLObjectType({
+	name: 'Todo',
+	fields: {
+		id: {
+			type: GraphQLInt
+		},
+		title: {
+			type: GraphQLString
+		}
+	}
+})
+
+const Root = new GraphQLObjectType({
+	name: 'Root',
 	fields: {
 		customer: {
 			type: CustomerType,
@@ -94,12 +124,31 @@ const RootQuery = new GraphQLObjectType({
 			resolve: (parentValue, args) => {
 				return PRODUCTS.filter(product => product.price <= args.price)
 			}
+		},
+		person: {
+			type: PersonType,
+			args: {
+				id: {
+					type: GraphQLString
+				}
+			},
+			resolve: (parentValue, args) => {
+				return axios.get(`http://localhost:3000/people/${args.id}`)
+					.then(res => res.data)
+			}
+		},
+		people: {
+			type: new GraphQLList(PersonType),
+			resolve: (parentValue, args) => {
+				return axios.get(`http://localhost:3000/people`)
+					.then(res => res.data)
+			}
 		}
 	}
 })
 
 const Schema = new GraphQLSchema({
-	query: RootQuery
+	query: Root
 })
 
 module.exports = Schema;
